5fcdab4c4a18bceac806396f59afdafd52c96c84
dev-kimseungwon <134674369+goosull@users.noreply.github.com>
2024-09-10 16:53:43 +0900

    카드 오류 수정

--- src/Card.js
+++ src/Card.js
@@ -6,10 +6,7 @@ const Card = ({ title, description }) => (
     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
       {title}
     </h5>
-    <p class="font-normal text-gray-700 dark:text-gray-400">
-     { Here are the biggest enterprise technology acquisitions of 2021 so far, in
-      reverse chronological order.}
-    </p>
+    <p class="font-normal text-gray-700 dark:text-gray-400">{description}</p>
   </a>
 );
 
